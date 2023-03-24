e2339c9f33498c28103b22eed6c8971b2cd9e462
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-24 19:42:45 +0700

    Prepare search engine

--- socialmediaweb/chrip/src/components/profileSide/LogoUse/Logo.js
+++ socialmediaweb/chrip/src/components/profileSide/LogoUse/Logo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import gardenmate from '../../../img/gardenmate.png';
-import searchicon from '../../../img/search.png';
+import searchicon from '../../../img/searchicon.png';
 import './Logo.css';
-
+import { UilSearch } from '@iconscout/react-unicons';
 
 function Logo() {
   return (
@@ -12,12 +12,41 @@ function Logo() {
     <div className='Search'>
         <input type='text' placeholder='Search'/>
         <div className='searchIcon'>
-          <img src={searchicon} alt='Search' />
+          <UilSearch/>
+
         </div>
       </div>
-   
+
   </div>
   );
 }
 
 export default Logo;
+
+
+// import React from 'react';
+// import { Icon } from '@iconscout/react-unicons';
+// import gardenmate from '../../../img/gardenmate.png';
+// import searchicon from '../../../img/searchicon.png';
+// import { InputGroup, FormControl, Button } from 'react-bootstrap';
+// import './Logo.css';
+
+// function Logo() {
+//     return (
+//         <div className='LogoUse'>
+//             <img src={gardenmate} alt="" />
+
+//             <InputGroup className='Search'>
+//                 <FormControl
+//                     placeholder='Search'
+//                     aria-label='Search'
+//                 />
+//                 <Button variant="light" className='searchButton'>
+//                     <Icon icon={searchicon} size={20} />
+//                 </Button>
+//             </InputGroup>
+//         </div>
+//     );
+// }
+
+// export default Logo;
