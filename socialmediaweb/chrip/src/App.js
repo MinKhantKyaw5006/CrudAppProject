000f392ecc96f1a9308bec8fb4c8b433797dce2e
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-24 14:17:28 +0700

    import  home component

--- socialmediaweb/chrip/src/App.js
+++ socialmediaweb/chrip/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
-import Test from './test';
+import Home from './pages/home/Home';
+
 function App() {
   return (
 
     <div className="App">
       <div className="blur" style={{ top: '-10%', right: '0' }}></div>
       <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
-      <Test/>
+      <Home/>
+     
     </div>
   );
 
