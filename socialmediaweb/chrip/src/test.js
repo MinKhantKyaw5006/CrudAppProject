256641dc4927e867e94e09b002347a3a0bb99d87
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-24 13:47:24 +0700

    fix files

---
+++ socialmediaweb/chrip/src/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <div>
+      <h1>This is a test component</h1>
+      <p>Welcome to the test component</p>
+    </div>
+  );
+}
+
+export default Test;
