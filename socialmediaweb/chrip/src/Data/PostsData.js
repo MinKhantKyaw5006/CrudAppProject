a67b9dd55e1ee80b6b70bd2e9d5a5ae12ae2a939
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-25 15:12:19 +0700

    Create dummy post data

---
+++ socialmediaweb/chrip/src/Data/PostsData.js
@@ -0,0 +1,37 @@
+import postPic1 from '../img/img1.jpg'
+import postPic2 from '../img/img2.jpg'
+import postPic3 from '../img/img3.jpg'
+import postPic4 from '../img/img4.jpg'
+
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Tzuyu',
+        desc: "Happy New Year all friends! #2023",
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Maryam',
+        desc: "Party time :)",
+        likes: 2300,
+        liked: false
+
+    },
+    {
+        img:postPic3,
+        name: "Salena Gomez",
+        desc: "At Archery Festival",
+        likes: 800,
+        liked: true
+    },
+    {
+        img:postPic4,
+        name: "Salena Gomez",
+        desc: "At Archery Festival",
+        likes: 800,
+        liked: false
+    }
+]
