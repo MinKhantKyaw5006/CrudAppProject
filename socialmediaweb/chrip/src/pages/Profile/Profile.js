69771be3c2c26ea7761793bc1eac1b005beef2f3
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-26 01:51:35 +0700

    dynamic second page profile test

---
+++ socialmediaweb/chrip/src/pages/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Profile.css';
+
+function Profile() {
+  return (
+    <div>
+      This is a example test component
+      Min Khant Kyaw 6410935
+    </div>
+  );
+}
+
+export default Profile;
