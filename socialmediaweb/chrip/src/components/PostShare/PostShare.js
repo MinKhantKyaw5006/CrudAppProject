354e3c64adcebd28b130c8271b1509835201ca1b
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-25 11:49:54 +0700

    photo,video,location,schedule

--- socialmediaweb/chrip/src/components/PostShare/PostShare.js
+++ socialmediaweb/chrip/src/components/PostShare/PostShare.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import './PostShare.css';
+import Profile from '../../img/profile.jfif'
+import { UilScenery, UilSchedule, UilPlayCircle, UilLocationPoint } from "@iconscout/react-unicons";
 
 function PostShare() {
-  return (
-    <div>
-      This is a example test component
-      Min Khant Kyaw 6410935
-    </div>
-  );
+    return (
+        <div className='PostShare'>
+            <img src={Profile} alt="" />
+
+            <div>
+                <input type="text" placeholder="What's happening" />
+                <div className='PostChoice'>
+                    <div className="option">
+                        <UilScenery />
+                        Photo
+                    </div>
+
+                    <div className="option">
+                        <UilPlayCircle />
+                        video
+                    </div>
+
+                    <div className="option">
+                        <UilLocationPoint />
+                        Location
+                    </div>
+
+                    <div className="option">
+                        <UilSchedule />
+                        Schedule
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+
+    );
 }
 
 export default PostShare;
