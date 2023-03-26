401b8ed3538e0179cb31eee61101b7ca9dc7231c
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-26 11:23:37 +0700

    authentication page finished

--- socialmediaweb/chrip/src/App.js
+++ socialmediaweb/chrip/src/App.js
@@ -14,7 +14,6 @@ function App() {
       {/* <Home/> */}
       {/* <Profile/> */}
       <Auth/>
-     
     </div>
   );
 
--- socialmediaweb/chrip/src/pages/Auth/Auth.js
+++ socialmediaweb/chrip/src/pages/Auth/Auth.js
@@ -1,14 +1,59 @@
 import React from 'react';
 import './Auth.css';
+import Logoimage from '../../img/gardenmate.png';
 
 function Auth() {
   return (
     <div className="Auth">
         <div className="a-left">
-            <img src="" alt="" />
+            <img src={Logoimage} alt="" />
+            <div className="Webname">
+              <h1>Chrip Social Media</h1>
+              <h5>  Bringing the world closer, with one connection at a time </h5>
+            </div>
         </div>
+        <Signup/>
     </div>
   );
+}
+
+function Signup(){
+  return(
+    <div className="a-right">
+     <form  className="infoForm">
+      <h3>Sign up</h3>
+      <div>
+        <input type="text" placeholder='First Name' className='infoInput'
+        name ='firstname' />
+      </div>
+      <div>
+        <input type="text" placeholder='Last Name' className='infoInput'
+        name ='lastname' />
+      </div>
+
+      <div>
+        <input type="text" placeholder='User Name' className='infoInput'
+        name ='username' />
+      </div>
+
+      <div>
+        <input type="text" placeholder='Enter Passwood' className='infoInput'
+        name ='passwood' />
+      </div>
+
+      <div>
+        <input type="text" placeholder='Confirm Passwood' className='infoInput'
+        name ='confrim' />
+      </div>
+      <div className="">
+        <span>Already have an account? Login!</span>
+      </div>
+      <button className="button infoButton" type="submit">SignUp</button>
+
+     </form>
+    </div>
+  )
+
 }
 
 export default Auth;
