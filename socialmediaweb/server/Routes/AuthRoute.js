a09494a1bd2798b118f9f26b6b3dd9171bc936ec
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-27 21:59:29 +0700

    auth controller post

--- socialmediaweb/server/Routes/AuthRoute.js
+++ socialmediaweb/server/Routes/AuthRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import { registerUser } from '../Controllers/AuthControllers.js';
 
 const router = express.Router()
 
 
-router.get('/', async(req,res)=>{res.send("Auth Route")})
+// router.get('/', async(req,res)=>{res.send("Auth Route")})
 
+//route for new user, login 
+
+router.post('/register', registerUser)
 export default router
