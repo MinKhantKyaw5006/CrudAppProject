86d19e67a85bea868edaa643d2b92fd4afe808c2
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-27 22:15:01 +0700

    test json data post

--- socialmediaweb/server/Controllers/AuthControllers.js
+++ socialmediaweb/server/Controllers/AuthControllers.js
@@ -1,18 +1,23 @@
 import UserModel from "../Models/userModel.js";
+import bcrypt from 'bcrypt';
 
 
 //new user registeration
-export const registerUser = async (req,res)=> {
-    const {username, passwood, firstname, lastname}=req.body;
+export const registerUser = async (req, res) => {
+    const { username, passwood, firstname, lastname } = req.body;
 
-    const newUser = new UserModel({username, passwood, firstname, lastname})
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPass = await bcrypt.hash(passwood, salt)
+
+    const newUser = new UserModel({ username, passwood: hashedPass, firstname, lastname })
 
     //to contact with server, use try catch
-    try{
+    try {
         await newUser.save()
         res.status(200).json(newUser)
-    } catch (error){
-        res.status(500).json({message: error.message})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
 
     }
 
