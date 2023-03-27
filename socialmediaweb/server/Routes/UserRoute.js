bf24720c69fd464de14572b1937bda2a7fd8427b
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-28 00:25:04 +0700

    Unfollow User

--- socialmediaweb/server/Routes/UserRoute.js
+++ socialmediaweb/server/Routes/UserRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, followUser, getUser, updateUser } from '../Controllers/UserControllers.js';
+import { deleteUser, followUser, getUser, UnFollowUser, updateUser } from '../Controllers/UserControllers.js';
 
 
 const router = express.Router();
@@ -13,5 +13,6 @@ router.get('/:id', getUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
 router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', UnFollowUser)
 
 export default router;
