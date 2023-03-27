bfa1cf7e5b12b4ae8bc0589492889569d3e81b5b
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-27 20:53:50 +0700

    connect atlas mongodb

--- socialmediaweb/server/index.js
+++ socialmediaweb/server/index.js
@@ -3,5 +3,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
 const app = express ();
-app.use(bodyParser.json({limit: '30mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+
+mongoose
+.connect("mongodb+srv://minkhantkyaw:6410935@clusterchrip.fv9nmdo.mongodb.net/chripdatabase?retryWrites=true&w=majority"
+, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>app.listen(5000,()=>console.log("Listening")))
