048e2d558ef6a6daf81446e27f9a1d503b03051b
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-29 01:20:16 +0700

    update

--- socialmediaweb/chrip/src/reducers/index.js
+++ socialmediaweb/chrip/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
 import authReducer from "./authReducers";
-import postReducer from "./postReducter";
+import postReducer from "./postReducer";
 
 export const reducers = combineReducers({authReducer, postReducer})
