05c32cae2ee68074d366332f0b1513c9e14c1ce7
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-29 10:56:15 +0700

    like post first test

--- socialmediaweb/chrip/src/components/Post/Post.js
+++ socialmediaweb/chrip/src/components/Post/Post.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Post.css';
 import Comment from '../../img/comment.png'
 import Share from '../../img/send.png'
 import Heart from '../../img/heartfill.png'
 import NotHeart from '../../img/heartempty.png'
 import { useSelector } from 'react-redux';
+import { likePost } from '../../api/PostRequest';
+
 
 function Post({data}) {
   const {user} = useSelector((state)=> state.authReducer.authData)
+  const [liked, setLiked] = useState(data.likes.includes(user._id))
+  const [likes, setLikes] = useState(data.likes.length)
+
+  const handleLike =()=>{
+    setLiked((prev)=>!prev)
+    likePost(data._id, user._id)
+    liked? setLikes((prev)=> prev-1): setLikes ((prev)=> prev+1)
+  }
+
   return (
    <div className="Post">
     <img src={data.image? process.env.REACT_APP_PUBLIC_FOLDER + data.image:""} alt="" />
@@ -15,13 +26,13 @@ function Post({data}) {
     {/*this will be reaction division with icons of 3*/}
 
     <div className="postReacts">
-        <img src={data.liked?Heart: NotHeart} alt="" />
+        <img src={liked?Heart: NotHeart} alt="" style={{cursor: "pointer"}} onClick={handleLike}/>
         <img src={Comment} alt="" />
         <img src={Share} alt="" />
 
     </div>
 
-    <span style={{color: "var(--gray)", fontSize: '12px'}}>{data.likes}Likes</span>
+    <span style={{color: "var(--gray)", fontSize: '12px'}}>{likes}Likes</span>
     <div className="detail">
         <span><b>{data.name}</b></span>
         <span>{data.desc}</span>
