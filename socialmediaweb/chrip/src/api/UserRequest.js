ba89c912a40b2a9cd61e15a12f9778eb3159ca01
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-29 23:23:12 +0700

    repushed all after fixing

--- socialmediaweb/chrip/src/api/UserRequest.js
+++ socialmediaweb/chrip/src/api/UserRequest.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 
 // const API = axios.create({ baseURL: 'http://localhost:5000' });
 
-// const API = axios.create({ baseURL: 'http://localhost:5000' });
-const API = axios.create({ baseURL: 'https://chripmedia-crud-c021dt5um-minkhantkyaw5006.vercel.app' });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+// const API = axios.create({ baseURL: 'https://chripmedia-crud-c021dt5um-minkhantkyaw5006.vercel.app' });
 // API.interceptors.request.use((req) => {
 //     if (localStorage.getItem('profile')) {
 //       req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
